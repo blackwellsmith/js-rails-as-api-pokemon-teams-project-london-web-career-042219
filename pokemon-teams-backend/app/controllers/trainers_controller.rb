accb2b7ded3ebe089f870954d664acfe1619340e
blackwellsmith <blackwellsmithiv@gmail.com>
2019-11-21 13:10:43 -0500

    Done.

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -2,6 +2,7 @@ class TrainersController < ApplicationController
 
     def index
         @trainers = Trainer.all
+        #binding.pry
         render json: @trainers.to_json
     end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,6 +1,7 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
 
 document.addEventListener('DOMContentLoaded', function () {
     fetch(TRAINERS_URL).then(res => res.json())
@@ -13,33 +14,57 @@ function renderTrainers(trainers) {
 }
 
 function trainerCard(trainer) {
-    const main = document.getElementsByTagName('main')
     const div = document.createElement('div')
     main.appendChild(div)
     div.className = "card"
-    div.setAttribute('data-id=', `${trainer.id}`)
+    div.setAttribute('data-id', `${trainer.id}`)
     const p = document.createElement('p')
+    div.appendChild(p)
     p.innerHTML = `${trainer.name}`
-    const button1 = document.createElement('button')
-    button.setAttribute('data-trainer-id=', `${trainer.id}`)
+    const button = document.createElement('button')
+    p.appendChild(button)
+    button.setAttribute('data-trainer-id', `${trainer.id}`)
     button.innerHTML = 'Add Pokemon'
     const ul = document.createElement('ul')
-    button.addEventListener('click', function (){
-        fetch(POKEMONS_URL).then(res => res.json())
-        .then( pokemons => renderPokemons(pokemons)) 
-    })
+    div.appendChild(ul)
+    fetch(POKEMONS_URL).then(res => res.json())
+        .then(pokemon => sorted(pokemon, trainer.id))
+        .then(result => renderPokemons(result))
+    .then(array => array.map(li => ul.appendChild(li)))
+
+   // button.addEventListener('click', function (){
+      //  fetch(POKEMONS_URL).then(res => res.json())
+     //   .then( pokemons => renderPokemons(pokemons)) 
+  //  })
+    
 }
 
 function renderPokemons(pokemons) {
-    pokemons.forEach(function (pokemon) {
-       pokemonCard(pokemon) 
+    let array = []
+    pokemons.forEach(function (poke) { 
+        array.push(pokemonCard(poke))
     })
+    return array
 }
 
 function pokemonCard(pokemon) {
     const li = document.createElement('li')
-    const button = document.createElent('button')
-    button.setAttribute("id", pokemon.id)
-    li.appendChild(button)
+    let btn = document.createElement('button')
+    btn.setAttribute("data-pokemon-id", pokemon.id)
+    btn.setAttribute("class", 'release')
+    btn.innerHTML = 'Release'
+    li.appendChild(btn)
     li.innerHTML = `${pokemon.nickname} (${pokemon.species})`
+    return li
 }
+
+function sorted(pokemon, trainerId) {
+    array = []
+    pokemon.forEach(function(poke){
+        if (poke.trainer_id == trainerId) {
+       array.push(poke)
+        }
+    })
+    return array
+}
+
